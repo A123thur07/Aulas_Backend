c80d55ebfbd198217b0410e7847e3346f2ba6e5b
Arthur Ferreira dos Santos <Cleyton123fs@gmail.com>
2025-09-30 15:53:58 -0300

    2.0

---
+++ FinanSESI/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const ejs = require('ejs')
+const expressLayouts = require('express-ejs-layouts')
+
+const app = express();
+
+// Configurando o motor de visualização EJS
+app.set('view engine', 'ejs');
+// Configurando a pasta para os arquivos estáticos
+app.use(express.static('public'));
+//Definindo o layout padrão
+app.use(expressLayouts);
+app.set('layout', 'layouts/principal');
+
+//Para processar e receber os dados do formulário
+    app.use(express.urlencoded({extend:true}))
+
+//Rota principal
+app.get('/', (req,res) => {
+    // Buscando o arquivo index.ejs na pasta views
+    res.render('index');
+});
+
+app.get('/sobre', (req,res) => {
+    // Buscando o arquivo sobre.ejs na pasta views
+    res.render('sobre');
+});
+app.get('/juros_Simples', (req,res) => {
+    // Buscando o arquivo sobre.ejs na pasta views
+    res.render('Juros_Simples');
+});
+app.post('/juros_simples', (req,res) => {
+    //recebendo os dados dos campos do formulário
+    const capital = Number(req.body.capital);
+    const taxa = Number(req.body.taxa);
+    const tempo = Number(req.body.tempo);
+    const juros =(capital*taxa*tempo)/100;
+    const total = juros+capital
+
+    res.render('juros_simples', {capital, taxa, tempo, juros, total})
+})
+
+
+const porta= 3000
+app.listen(porta, () => {
+    console.log(`Servidor http://localhost:${porta}`);
+})
+
+
+
