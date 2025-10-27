84fabc52edc96e1628c0de2e1d40cb1736318057
Arthur Ferreira dos Santos <Cleyton123fs@gmail.com>
2025-10-27 12:04:45 -0300

    7

--- Escola/routes/professores.js
+++ Escola/routes/professores.js
@@ -18,9 +18,43 @@ rotas.post('/novo', async (req,res) =>{
     const telefone= req.body.telefone;
     const formacao= req.body.formacao;
     // OU const{ nome_professor, telefone, formacao} = req.body;
+    //Inserindo dados recebidos no BD
     const sql= `INSERT INTO professores (nome_professor, telefone, formacao) 
     VALUES($1, $2, $3)`;
     await BD.query(sql, [nome_professor, telefone, formacao]);
+    //Redirecionando para a página de listagem de professores
+    res.redirect('/professores/listar')
+});
+//Criando rota para excluir os professor passando o id (D-delete)
+rotas.post('/excluir/:id', async (req,res)=> {
+    const id = req.params.id;
+    const sql = 'UPDATE professores SET ativo = false WHERE id_professor = $1';
+    await BD.query(sql,[id]);
+    res.redirect('/professores/listar')
+});
+rotas.get('/editar/:id', async (req,res) => {
+    //Recebendo o código que quero excluir
+    const id = req.params.id;
+    const sql = 'SELECT * FROM professores WHERE id_professor = $1'
+    const dados = await BD.query(sql, [id]);
+    console.log(dados.rows[0]);
+    res.render('professores/editar.ejs',{professor: dados.rows[0]})
+});
+rotas.post('/editar/:id', async (req,res) =>{
+    const id= req.params.id;
+    //Obtendo os dados do formulário e cadastro e guardando uma variável
+    const nome_professor= req.body.nome_professor;
+    const telefone= req.body.telefone;
+    const formacao= req.body.formacao;
+    // OU const{ nome_professor, telefone, formacao} = req.body;
+    //Inserindo dados recebidos no BD
+    const sql= `UPDATE professores SET 
+    nome_professor = $1,
+    telefone = $2,
+    formacao = $3
+    WHERE id_professor = $4`;
+    await BD.query(sql, [nome_professor, telefone, formacao, id]);
+    //Redirecionando para a página de listagem de professores
     res.redirect('/professores/listar')
 });
 module.exports = rotas;
--- Escola/routes/turmas.js
+++ Escola/routes/turmas.js
@@ -6,20 +6,47 @@ const BD = require('../db')
 rotas.get('/listar', async (req,res) =>{
     const dados = await BD.query('SELECT * FROM turmas order by nome_turma');
     console.log(dados.rows);
-    res.render('turmas/lista.ejs', {dadosTurmas: dados.rows});
+    res.render('turmas/lista', {dadosTurmas: dados.rows});
 });
 rotas.get('/novo', async (req,res) =>{
     res.render('turmas/novo')
 });
 rotas.post('/novo', async (req,res) =>{
     //Obtendo os dados do formulário e cadastro e guardando uma variável
-    const nome_professor= req.body.nome_professor;
-    const telefone= req.body.telefone;
-    const formacao= req.body.formacao;
+    const nome_turma= req.body.nome_turma;
+    // OU const{ nome_turma} = req.body;
+    const sql= `INSERT INTO turmas (nome_turma) 
+    VALUES($1)`;
+    await BD.query(sql, [nome_turma]);
+    res.redirect('/turmas/listar')
+});
+//Criando rota para excluir os professor passando o id (D-delete)
+rotas.post('/excluir/:id', async (req,res)=> {
+    const id = req.params.id;
+    //a melhor prática e desativar o item
+    const sql = 'UPDATE turmas SET ativo = false WHERE id_turma = $1';
+    await BD.query(sql,[id]);
+    res.redirect('/turmas/listar')
+})
+rotas.get('/editar/:id', async (req,res) => {
+    //Recebendo o código que quero excluir
+    const id = req.params.id;
+    const sql = 'SELECT * FROM turmas WHERE id_turma = $1'
+    const dados = await BD.query(sql, [id]);
+    console.log(dados.rows[0]);
+    res.render('turmas/editar.ejs',{turma: dados.rows[0]})
+});
+rotas.post('/editar/:id', async (req,res) =>{
+    const id= req.params.id;
+    //Obtendo os dados do formulário e cadastro e guardando uma variável
+    const nome_turma= req.body.nome_turma;
     // OU const{ nome_professor, telefone, formacao} = req.body;
-    const sql= `INSERT INTO professores (nome_professor, telefone, formacao) 
-    VALUES($1, $2, $3)`;
-    await BD.query(sql, [nome_professor, telefone, formacao]);
-    res.redirect('/professores/listar')
+    //Inserindo dados recebidos no BD
+    const sql= `UPDATE turmas SET 
+    nome_turma = $1
+    WHERE id_turma = $2`;
+    await BD.query(sql, [nome_turma, id]);
+    //Redirecionando para a página de listagem de professores
+    res.redirect('/turmas/listar')
 });
 module.exports = rotas;
